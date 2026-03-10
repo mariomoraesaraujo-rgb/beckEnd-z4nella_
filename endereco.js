dafcb372aee745def5f4698bb39d861077bf1033
mariomoraesaraujo-rgb <mario.moraes.araujo@escola.pr.gov.br>
2026-03-10 14:38:02 -0300

    Add files via upload

---
+++ endereco.js
@@ -0,0 +1,9 @@
+console.log("Endereço completo");
+
+let rua  = "Rua aldo pigozzi";
+let numero = 7;
+let cidade = "Curitiba";
+
+let endereco = rua + ", " + numero + "-" + cidade;
+//console.log(endereco);
+console.log(" O endereço completo é: " + endereco);
