3b23d0f952cd25cf8fed73a0387760c505c34370
mariomoraesaraujo-rgb <mario.moraes.araujo@escola.pr.gov.br>
2026-03-25 18:29:23 +0000

    versao1

---
+++ produto.js
@@ -0,0 +1,9 @@
+const produto = "Mochila Impermeável";
+let preco = 189.90;
+let estoque = 14;
+const categoria = "acessorio";
+
+console.log(`O produto é: ${produto}.`);
+console.log(`A ${produto} custa R$ ${preco.toFixed(2)}.`);
+console.log(`o estoque ainda possui ${estoque} mochilas.`);
+console.log(`a categoria de ${produto} é ${categoria}.`);
