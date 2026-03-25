5e03aeb8fcbfeb64749930e3754d285db47ea20b
mariomoraesaraujo-rgb <mario.moraes.araujo@escola.pr.gov.br>
2026-03-25 18:54:30 +0000

    versao2

--- temperatura.js
+++ temperatura.js
@@ -4,3 +4,7 @@ let tempCelcios = 18.5;
 
 const tempFahrenheit = tempCelcios * (9/5) + 32;
 const tempKelvin = tempCelcios + 273.15;
+
+console.log(`A temperatura de ${cidade} está ${tempCelcios} Cº`);
+console.log(` Fahrenheit: ${tempFahrenheit.toFixed(1)} Fº`);
+console.log(`Kelvin: ${tempKelvin.toFixed(1)} Kº`);
