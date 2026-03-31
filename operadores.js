e96214bfc879589bb8d6e82cc5e5165ecd1e67d4
mariomoraesaraujo-rgb <mario.moraes.araujo@escola.pr.gov.br>
2026-03-31 17:39:14 +0000

    versao1

---
+++ operadores.js
@@ -0,0 +1,29 @@
+//OPERADOR E &&
+let maiorIdade = true;
+let possuiDocumento = true;
+
+if (maiorIdade && possuiDocumento) {
+    console.log("pode entrar na festa!")
+}else{
+    console.log("não pode enttrar na festa.")
+
+};
+
+//OPERADOR OU ||
+let cartaoCredito = true;
+let dinheiro = true;
+
+if (cartaoCredito || dinheiro){
+    console.log("pode fazer a compra.")
+}else{
+    console.log("não pode fazer a compra.")
+};
+
+//OPERADORES NOT !
+let estaChovendo = true;
+
+if (!estaChovendo){
+    console.log("pode sair sem guarda-chuva.")
+}else{
+    console.log("leve o guarda-chuva.")
+};
